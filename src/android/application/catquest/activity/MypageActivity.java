c60cbfd4fc85c814a453da51a198aa4b852371d0
U-KAEDE\やす <@kaede.(none)>
2013-09-10 22:16:21 +0900

    マイページ新規作成

---
+++ src/android/application/catquest/activity/MypageActivity.java
@@ -0,0 +1,13 @@
+package android.application.catquest.activity;
+
+public class MypageActivity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
