4c0ed51f8c839d73be5ab8e0e8620ed90db86e92
U-KAEDE\やす <@kaede.(none)>
2013-09-21 22:52:02 +0900

    ページにボタン追加

--- src/android/application/catquest/activity/GachaTopActivity.java
+++ src/android/application/catquest/activity/GachaTopActivity.java
@@ -1,5 +1,44 @@
 package android.application.catquest.activity;
 
-public class GachaTopActivity {
+import android.app.Activity;
+import android.application.catquest.R;
+import android.application.catquest.activity.LoginActivity.ButtonClickListener;
+import android.application.catquest.dao.DBAccessOpenHelper;
+import android.content.ContentValues;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 
+public class GachaTopActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_activity_layout);
+		//ボタンオブオブジェクト取得
+		Button button =(Button)findViewById(R.id.button2);
+		button.setOnClickListener(new ButtonClickListener());
+
+	}
+
+	class ButtonClickListener implements OnClickListener{
+		public void onClick(View v){
+
+			Intent intent = new Intent(LoginActivity.this, MypageActivity.class);
+			intent.putExtra("ASIN", "tag");
+			startActivity(intent);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
